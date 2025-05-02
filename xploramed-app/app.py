6fb730e7218b441e441c9e28ff8e80756837223b
xploramed1 <xploramed@gmail.com>
2025-05-02 20:13:43 +0300

    Update app.py

--- xploramed-app/app.py
+++ xploramed-app/app.py
@@ -15,7 +15,16 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow_addons.layers import InstanceNormalization
 from skimage.transform import resize
 import pandas as pd
+import gdown
 
+
+def ensure_model_exists():
+    model_path = "DeepSeg_model.hdf5"
+    if not os.path.exists(model_path):
+        print("Downloading model from Google Drive...")
+        url = "https://drive.google.com/uc?id=1jtveR5q1AdmOnqPEGonXC8TMv3hQIMZK"
+        gdown.download(url, model_path, quiet=False)
+        
 # --------------------- NeuroXAI Model + Utils ---------------------
 def norm_image(img, NORM_TYP="norm"):
     if NORM_TYP == "standard_norm":
@@ -99,6 +108,7 @@ if uploaded_files and len(uploaded_files) == 4:
 
         if st.button("Run Segmentation + Grad-CAM"):
             with st.spinner("Running model..."):
+                ensure_model_exists()
                 model = get_deepseg(WEIGHTS="DeepSeg_model.hdf5")
                 io_imgs = load_images(ID=patient_id, PATH_DATA=tmp_dir)
                 preds = model(io_imgs, training=False).numpy()
